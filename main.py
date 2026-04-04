718f56f6340044805ead7e26744a4f3125fe9b80
Ruslan <rusiksadun@gmail.com>
2026-04-04 19:59:14 +0300

    Добавил patch и delete для tasks

--- main.py
+++ main.py
@@ -27,6 +27,11 @@ class TaskCreate(BaseModel):
     title: str
 
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
+
 class BookCreate(BaseModel):
     book: str
 
@@ -48,6 +53,32 @@ def create_task(payload: TaskCreate):
     tasks.append(task)
     return task
 
+
+@app.patch("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: str, payload: TaskUpdate):
+    """
+    Обновить существующую задачу
+    task_id получаем из url
+    payload получаем из тела запроса
+    """
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title is not None:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Задача не найдена")
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str):
+    """Удалить задачу"""
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return
 
 @app.get("/book")
 def get_book():
