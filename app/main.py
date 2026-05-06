2a13777cb6afddd0b2f7654350606de5d134513b
Ruslan <rusiksadun@gmail.com>
2026-05-06 14:07:10 +0300

    feat: добавить миграции alembic

--- app/main.py
+++ app/main.py
@@ -1,24 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from contextlib import asynccontextmanager
-
 from app.api.router import api_router
 from app.core.config import get_settings
-from app.models.base import Base
-from app.models.task import TaskORM
-from app.models.category import CategoryORM
-from app.db.session import engine
-
-
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
 
 
 settings = get_settings()
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 app.include_router(api_router)
 
 
